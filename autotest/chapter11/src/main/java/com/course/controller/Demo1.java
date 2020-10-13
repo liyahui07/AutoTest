e98410fae692674acd965010467d9ab258eaa5d9
liyahui07 <1814022216@qq.com>
2020-10-13 23:35:56 +0800

    chapter 11

---
+++ autotest/chapter11/src/main/java/com/course/controller/Demo1.java
@@ -0,0 +1,20 @@
+package com.course.controller;
+
+import io.swagger.annotations.Api;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(value = "/",description = "demo1")
+public class Demo1 {
+    @Autowired
+    private SqlSessionTemplate sessionTemplate;
+
+    @GetMapping("/getUserCount")
+    public Integer getUserCount(){
+        int i = sessionTemplate.selectOne("selectUC");
+        return i;
+    }
+}
